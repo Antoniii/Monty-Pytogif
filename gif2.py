23185e2c2096a68e9d6719849642f009f986e159
Thonny van A'dams <anto-kha1@mail.ru>
2021-12-22 16:16:53 +0300

    add files

---
+++ gif2.py
@@ -0,0 +1,21 @@
+from PIL import Image
+ 
+# Список для хранения кадров.
+frames = []
+ 
+for frame_number in range(1, 11):
+    # Открываем изображение каждого кадра.
+    #frame = Image.open(f'homer/homer-{frame_number}.jpg')
+    frame = Image.open(f'drake/drakeno-{frame_number}.png')
+    # Добавляем кадр в список с кадрами.
+    frames.append(frame)
+ 
+# Берем первый кадр и в него добавляем оставшееся кадры.
+frames[0].save(
+    'drake.gif',
+    save_all=True,
+    append_images=frames[1:],  # Срез который игнорирует первый кадр.
+    optimize=True,
+    duration=150,
+    loop=0
+)
